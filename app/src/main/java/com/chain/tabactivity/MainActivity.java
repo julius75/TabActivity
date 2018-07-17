bf2f18776cab87af2b2f41c76f8a3b0b89229928
julius75 <juliusnchagwa75@gmail.com>
2018-07-17 23:21:31 +0300

    post layouts

--- app/src/main/java/com/chain/tabactivity/MainActivity.java
+++ app/src/main/java/com/chain/tabactivity/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
@@ -31,5 +34,22 @@ public class MainActivity extends AppCompatActivity {
 
         tabLayout.getTabAt(0).setIcon(R.drawable.suggest);
         tabLayout.getTabAt(1).setIcon(R.drawable.idea);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == R.id.logout){
+            Toast.makeText(this, "signOut", Toast.LENGTH_SHORT).show();
+
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+//menu bar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
